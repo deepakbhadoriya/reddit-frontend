fd7757b357910b4a633cc284990aeb18e1b5ee3f
Deepak Bhadoriya <deepakkbhadoriya@gmail.com>
2021-08-05 00:14:18 +0530

    feat: login & signup done

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Flex, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
+
+interface NavBarProps {}
+const NavBar: React.FC<NavBarProps> = () => {
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href='/login'>
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href='/register'>
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <>
+        {data.me.username}
+        <Button ml={3} variant='link'>
+          Logout
+        </Button>
+      </>
+    );
+  }
+
+  return (
+    <Flex bg='tomato' p={4} ml={'auto'} justifyContent='flex-end'>
+      {body}
+    </Flex>
+  );
+};
+export default NavBar;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,13 +5,13 @@ import { Box, Button } from '@chakra-ui/react';
 
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 
-interface registerProps {}
+interface loginProps {}
 
-const Register: React.FC<registerProps> = () => {
-  const [{ data }, register] = useRegisterMutation();
+const Login: React.FC<loginProps> = () => {
+  const [{ data }, login] = useLoginMutation();
   const router = useRouter();
 
   return (
@@ -19,10 +19,10 @@ const Register: React.FC<registerProps> = () => {
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
             // worked
             router.push('/');
           }
@@ -34,7 +34,7 @@ const Register: React.FC<registerProps> = () => {
               <InputField name='password' placeholder='password' label='Password' type='password' />
             </Box>
             <Button mt={4} type='submit' colorScheme='teal' isLoading={isSubmitting}>
-              Register
+              Login
             </Button>
           </Form>
         )}
@@ -43,13 +43,4 @@ const Register: React.FC<registerProps> = () => {
   );
 };
 
-export default Register;
-
-// * Snippet
-// import React from 'react'
-// interface registerProps{
-// }
-// const register:React.FC<registerProps> = () => {
-//   return (<div></div>)
-// }
-// export default register
+export default Login;
