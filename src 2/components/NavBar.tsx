533753cde5fec38a83eeb393f297a2fff4819857
Deepak Bhadoriya <deepakkbhadoriya@gmail.com>
2021-08-11 23:57:21 +0530

    feat: created post done

---
+++ src 2/components/NavBar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, Flex, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { useLogoutMutation, useMeQuery } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
+
+interface NavBarProps {}
+const NavBar: React.FC<NavBarProps> = () => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+  const [{ data, fetching }] = useMeQuery({ pause: isServer() });
+
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href='/login'>
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href='/register'>
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <>
+        {data.me.username}
+        <Button ml={3} variant='link' onClick={() => logout()} isLoading={logoutFetching}>
+          Logout
+        </Button>
+      </>
+    );
+  }
+
+  return (
+    <Flex bg='tomato' p={4} ml={'auto'} justifyContent='flex-end'>
+      {body}
+    </Flex>
+  );
+};
+
+export default NavBar;
